3ae368d50ef632551b58841192420a0011253c53
Anton Kostylov <antoniok003@gmail.com>
2021-04-16 11:40:27 +0300

    final version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,29 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const advantagesNext = document.querySelector('.carousel__right');
   const advantagesPrevious = document.querySelector('.carousel__left');
   const advantagesSlidesField = document.querySelector('#advantages');
-  const advantagesWidth = window.getComputedStyle(advantagesSlidesField).width;
   const advantagesCurrent = document.querySelector('#current');
   const techNext = document.querySelector('#right');
   const techPrevious = document.querySelector('#left');
   const techCurrent = document.querySelector('#current-tech');
   const techSlidesField = document.querySelector('#technology');
-  const techWidth = window.getComputedStyle(techSlidesField).width;
   const featuresNext = document.querySelector('#next');
   const featuresPrevious = document.querySelector('#prev');
   const featuresCurrent = document.querySelector('#current-featuresh');
   const featuresSlidesField = document.querySelector('#features');
-  const featuresWidth = window.getComputedStyle(featuresSlidesField).width;
-
-  let move = 0;
-  let slideIndex = 1;
-
-  function current(slide) {
-    slide.textContent = `0${slideIndex}`;
-  }
-
-  current(advantagesCurrent);
-  current(techCurrent);
-  current(featuresCurrent);
 
   openButton.addEventListener('click', () => {
     menu.classList.toggle('menu--move');
@@ -36,69 +22,95 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   );
 
-  function deleteNotDigits(str) {
-    return Math.round(str.replace(/px/g, ''));
-  }
+  function nextSlide(right, left, slide, slidesField, slidesCount) {
+    let move = 0;
+    let slideIndex = 1;
 
-  function nextSlide(right, left, width, slide, slidesField, slidesCount) {
     right.addEventListener('click', () => {
-      if (move === deleteNotDigits(width) * (slidesCount - 1)) {
+      const width = slidesField.clientWidth;
+
+      if (move === width * (slidesCount - 1)) {
         move = 0;
         slideIndex = 1;
+      } else {
+        move += width;
+        slideIndex++;
+      }
+
+      if (slideIndex === slidesCount) {
         right.style.borderBottomColor = '#BDBDBD';
         right.style.borderRightColor = '#BDBDBD';
-        left.style.borderTopColor = '#BDBDBD';
-        left.style.borderLeftColor = '#BDBDBD';
-      } else {
-        move += deleteNotDigits(width);
-        slideIndex++;
-        right.style.borderBottomColor = '#333';
-        right.style.borderRightColor = '#333';
         left.style.borderTopColor = '#333';
         left.style.borderLeftColor = '#333';
+      } else if (slideIndex === 1) {
+        left.style.borderTopColor = '#BDBDBD';
+        left.style.borderLeftColor = '#BDBDBD';
+        right.style.borderBottomColor = '#333';
+        right.style.borderRightColor = '#333';
+      } else {
+        left.style.borderTopColor = '#333';
+        left.style.borderLeftColor = '#333';
+        right.style.borderBottomColor = '#333';
+        right.style.borderRightColor = '#333';
       }
-      current(slide);
+
+      slide.textContent = `0${slideIndex}`;
       slidesField.style.transform = `translateX(-${move}px)`;
     });
   }
 
-  nextSlide(advantagesNext, advantagesPrevious, advantagesWidth,
+  nextSlide(advantagesNext, advantagesPrevious,
     advantagesCurrent, advantagesSlidesField, 4);
-  nextSlide(techNext, techPrevious, techWidth, techCurrent, techSlidesField, 2);
+  nextSlide(techNext, techPrevious, techCurrent, techSlidesField, 2);
 
-  nextSlide(featuresNext, featuresPrevious, featuresWidth,
+  nextSlide(featuresNext, featuresPrevious,
 
     featuresCurrent, featuresSlidesField, 3);
 
-  function previousSlide(left, right, width, slide, slidesField, slidesCount) {
+  function previousSlide(left, right, slide, slidesField, slidesCount) {
+    let move = 0;
+    let slideIndex = 1;
+
     left.addEventListener('click', () => {
+      const width = slidesField.clientWidth;
+
       if (move === 0) {
-        move = deleteNotDigits(width) * (slidesCount - 1);
+        move = width * (slidesCount - 1);
         slideIndex = slidesCount;
-        left.style.borderTopColor = '#BDBDBD';
-        left.style.borderLeftColor = '#BDBDBD';
+      } else {
+        move -= width;
+        slideIndex--;
+      }
+
+      if (slideIndex === slidesCount) {
         right.style.borderBottomColor = '#BDBDBD';
         right.style.borderRightColor = '#BDBDBD';
+        left.style.borderTopColor = '#333';
+        left.style.borderLeftColor = '#333';
+      } else if (slideIndex === 1) {
+        left.style.borderTopColor = '#BDBDBD';
+        left.style.borderLeftColor = '#BDBDBD';
+        right.style.borderBottomColor = '#333';
+        right.style.borderRightColor = '#333';
       } else {
-        move -= deleteNotDigits(width);
-        slideIndex--;
         left.style.borderTopColor = '#333';
         left.style.borderLeftColor = '#333';
         right.style.borderBottomColor = '#333';
         right.style.borderRightColor = '#333';
       }
-      current(slide);
+
+      slide.textContent = `0${slideIndex}`;
       slidesField.style.transform = `translateX(-${move}px)`;
     });
   }
 
-  previousSlide(advantagesPrevious, advantagesNext, advantagesWidth,
+  previousSlide(advantagesPrevious, advantagesNext,
     advantagesCurrent, advantagesSlidesField, 4);
 
-  previousSlide(techPrevious, techNext, techWidth,
+  previousSlide(techPrevious, techNext,
 
     techCurrent, techSlidesField, 2);
 
-  previousSlide(featuresPrevious, featuresNext, featuresWidth,
+  previousSlide(featuresPrevious, featuresNext,
     featuresCurrent, featuresSlidesField, 3);
 });
